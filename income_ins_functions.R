f5d712e6437ff800590066d395e8107982563421
shanejorr <shane.j.orr@gmail.com>
2018-10-30 13:54:56 -0400

    update income insufficiency

--- income_ins_functions.R
+++ income_ins_functions.R
@@ -128,3 +128,152 @@ ff_inflation_adj_table <- function(year_adjust) {
   
   return(yearly_cpi)
 }
+
+create_economic_units <- function(con, year, state) {
+  
+  # this function imports population level PUMA data and creates economic units
+  
+  ##### calculate economic unit labels ###############
+  
+  # An economic unit is the reference person and everyone with a relationship to the reference person.
+  # This includes spouses, partners, and any children in the house (whether related or not)
+  # All people in the household outside this group are assigned their own ecenomic unit
+  
+  # economic units are derived from the relationship variable
+  # these variables changed coding in 2008;
+  # therefore we need two objects representing these variables, depending on year
+  econonmic_unit_a <- c(0,1,2,3,4,5,6,7,10,11)
+  econonmic_unit_b <- c(0,1,2,3,4,5,6,7,8,9,10,13,14,15)
+  
+  economic_unit_vec <- if (year < 2008) economic_unit_a else econonmic_unit_b
+  
+  ###############################################
+  
+  # since estimated expenses are at the county level, we need to add counties
+  counties <- read_csv('puma_counties.csv')
+  
+  # select PUMA area code based on year
+  if (!!year < 2011) {
+    
+    counties <- counties %>%
+      select(puma2k, cntyname) %>%
+      rename(PUMA = puma2K)
+    
+  } else {
+    
+    counties <- counties %>%
+      select(puma12, cntyname) %>%
+      rename(PUMA = puma12)
+     
+  }
+  
+  # remove duplicate PUMAs
+  # this will remove some counties since counties can span more than one PUMA
+  counties <- counties %>%
+    distinct(PUMA, .keep_all = TRUE) %>%
+    # remove ' NC' from county name
+    mutate(cntyname = str_replace_all(cntyname, ' NC', ''))
+  
+  # create table name by extracting last two digits from year
+  # and placing after 'p_'
+  yr <-str_extract(as.character(year), '[0-9][0-9]$')
+  table_name <- paste0('p_', yr)
+  
+  # connect to database table
+  population <- tbl(con, table_name)
+  
+  pop_vars <- c('SERIALNO', # serial number grouped by household 
+                'SPORDER', # order of person in household
+                'ST', # state (needed to calculate state income taxes)
+                'PUMA', # Public use micro area
+                'year',
+                # relationship to reference person; variable name changed in 2010
+                ifelse(year < 2010, 'REL', 'RELP'),
+                'PINCP', # total personal income
+                'AGEP', #age
+                'ESR' # employment status
+  )
+  
+  # import population data
+  pop <- population %>%
+    select(!!pop_vars) %>%
+    # need to collect now because cannot transform NA without collecting
+    filter(ST == !!state,
+           PUMA %in% c(1802)) %>%
+    collect() %>%
+    # add county names
+    left_join(counties, by = 'PUMA')
+  
+  # change REL column name to RELP if REL is a column (less than 2010)
+  # this allows us to use the same column names for all years
+  pop <- if ('REL' %in% colnames(pop)) rename(pop, RELP = REL) else pop
+  
+  pop <- pop %>%
+    # replace NA values for income with zero
+    mutate(PINCP = replace_na(PINCP, 0),
+           # create boolean of true or false based on whether person 
+           # is in economic unit with reference person
+           economic_unit = ifelse(RELP %in% !!economic_unit_vec, TRUE, FALSE),
+           # if the year is 2017, remove the 2017 from the start of the SERIALNO
+           SERIALNO = if (!!year == 2017) as.integer(str_replace_all(.$SERIALNO, '^2017', '')) else .$SERIALNO)
+  
+  return(pop)
+  
+}
+
+rent <- function(pop) {
+  
+  ###############################################################
+  # Calculate fair market rent per household 
+  # based on the total number of bedrooms needed in house
+  #
+  # Reference person and spouse get one bedroom
+  # Every other adult gets their own bedroom
+  # Up to two children (under 18) can share a room
+  # 4 is the max number of bedrooms
+  #
+  # rent between household and economic unit is shared
+  # based on the proportion of rooms each economic unit needs
+  #############################################################
+  
+  # import rent values
+  fmr <- read_csv('data/cleaned_data/fmr.csv') %>%
+    # convert to long form where each row is a different year, county and bedroom number
+    # this will allow for easy merge with PUMA dataset based on number of bedrooms
+    gather(house_bedrooms, rent, -year, -countyname) %>%
+    # remove 'fmr' from column showing number of bedrooms
+    # needed so we can merge on number of bedrooms
+    mutate(house_bedrooms = as.integer(str_replace_all(house_bedrooms, 'fmr', '')))
+  
+  # calculate numer of bedrooms each person needs
+  pop <- pop %>%
+    mutate(bedrooms = ifelse(
+      # since children share rooms, they need 0.5
+      AGEP < 18, 0.5,
+      # spuse shares bedroom, so he/she does not have one
+      ifelse(RELP == 1, 0,
+             # all other adults get one
+             1)
+    )) %>%
+    # calculate number of bedrooms in household by grouping by bedrooms
+    # and summing total number of individual bedrooms
+    group_by(SERIALNO) %>%
+    # use ceiling to round up
+    mutate(house_bedrooms = ceiling(sum(bedrooms)),
+           # calculate each person's proportion of rent
+           rent_prop = bedrooms / house_bedrooms,
+           # cannot have more than 4 bedrooms
+           house_bedrooms = ifelse(house_bedrooms > 4, 4, house_bedrooms)) %>%
+    # add rent amounts
+    left_join(fmr, by = c('year', 'cntyname' = 'countyname', 'house_bedrooms')) %>%
+    # calculate each individual's proportion of rent
+    mutate(ind_rent = rent * rent_prop) %>%
+    # calculate each economic unit's proportion of rent
+    group_by(SERIALNO, economic_unit) %>%
+    mutate(ecnonomic_unit_rent = ifelse(economic_unit == TRUE,
+                                        sum(ind_rent),
+                                        ind_rent)) %>%
+    select(-bedrooms:-ind_rent) %>%
+    ungroup()
+}
+  
--- income_insufficiency.R
+++ income_insufficiency.R
@@ -1,46 +1,13 @@
 library(tidyverse)
 library(DBI)
 
+source('income_ins_functions.R')
+
 con <- dbConnect(RSQLite::SQLite(), "pums_db.db")
 
-# con: database connection
-# year: year to pull pums
 
-# establish connection to tables
-year <- 2017
-# create table name by extracting last two digits from year
-# and placing after 'p_'
-yr <-str_extract(as.character(year), '[0-9][0-9]$')
-table_name <- paste0('p_', yr)
 
-# connect to database table
-population <- tbl(con, table_name)
+# import needed PUMA data and create economic units
+pop <- create_economic_units(con, 2017, 37) %>%
+  rent()
 
-pop_vars <- c('SERIALNO', # serial number grouped by household 
-              'SPORDER', # order of person in household
-              'ST', # state (needed to calculate state income taxes)
-              'PUMA', # Public use micro area
-              # relationship to reference person; variable name changed in 2010
-              ifelse(year < 2010, 'REL', 'RELP'),
-              'PINCP' # total personal income
-              )
-
-# import population data
-pop <- population %>%
-  select(!!pop_vars) %>%
-  # need to collect now because cannot transform NA without collecting
-  filter(ST == 37,
-         PUMA == 1801) %>%
-  collect()
-
-### calculate economic units
-
-# An economic unit is the reference person and everyone with a relationship to the reference person.
-# This includes spouses, partners, and any children in the house (whether related or not)
-# All people in the household outside this group are assigned their own ecenomic unit
-
-# economic units are derived from the relationship variable
-# these variables are changed coding in 2008;
-# therefore we need two objects representing these variables, depending on year
-econonmic_unit_a <- c(0,1,2,3,4,5,6,7,10,11)
-econonmic_unit_a <- c(0,1,2,3,4,5,6,7,8,9,10,13,14,15)
