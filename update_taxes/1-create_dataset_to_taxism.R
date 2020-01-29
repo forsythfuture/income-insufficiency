6721fb7a8ae0dbb64c9fec90bebe00d3dcfd4223
Shane Orr <shane.j.orr@gmail.com>
2020-01-29 14:14:28 -0500

    copy 2018 taxsim output

--- update_taxes/1-create_dataset_to_taxism.R
+++ update_taxes/1-create_dataset_to_taxism.R
@@ -21,6 +21,23 @@ pop_file <- glue('https://censuspums.s3.amazonaws.com/oneyear/nc_pop/ss{year_fil
 # calculate taxable income and write out results
 tax_df <- pop_taxes(pop_file, current_year)
 
+# TAXSIM ID's cannot have letters, so take out letters of serial numbers, but save so we can reinsert
+# also take out first four letters
+
+# map to each serial number an integer
+serial_num_mapping <- data.frame(
+  'SERIALNO' = tax_df$SERIALNO,
+  'id' = seq(1, nrow(tax_df)),
+  'year' = current_year
+)
+
+# replace serial number with integer
+tax_df$SERIALNO <- serial_num_mapping$id
+
 # create file name of output based on year
 file_name <- glue('update_taxes/nc_to_taxsim_online/to_tasxim_{year_file}.csv')
 write_csv(tax_df, file_name, col_names = FALSE)
+
+# write out mapping of serial number and id data frame
+write_rds(serial_num_mapping, glue('update_taxes/nc_from_taxsim_online/serial_num_mapping_{year_file}.rds'))
+  
